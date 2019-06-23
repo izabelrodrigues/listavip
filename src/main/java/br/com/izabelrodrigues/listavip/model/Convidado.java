1cb40570686b68dbf39c91143890be1708aadcd8
izabelrodrigues <izabel.castro.rodrigues@gmail.com>
2019-06-23 20:52:04 -0300

    Modificando mapeamento para postgresql e adicionando contrutor com parâmetros

--- src/main/java/br/com/izabelrodrigues/listavip/model/Convidado.java
+++ src/main/java/br/com/izabelrodrigues/listavip/model/Convidado.java
@@ -2,18 +2,30 @@ package br.com.izabelrodrigues.listavip.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import br.com.izabelrodrigues.listavip.Constants;
 
 @Entity(name = "convidado")
 public class Convidado {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.AUTO, generator=Constants.CONVIDADO_SEQUENCE)
 	private Long id;
 
 	private String nome;
 	private String email;
 	private String telefone;
+
+	public Convidado() {
+	}
+
+	public Convidado(String nome, String email, String telefone) {
+		setNome(nome);
+		setEmail(email);
+		setTelefone(telefone);
+	}
 
 	public Long getId() {
 		return id;
